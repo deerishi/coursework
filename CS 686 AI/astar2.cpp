fc3bc98eedc278a6aa45fa51f738d49e4236dbc6
deerishi <deerishi@gmail.com>
2016-01-21 14:57:56 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -340,7 +340,7 @@ int main()
         cout<<*it<<" ";
     }
     int initialHeuristic=calculateHeuristic(v1,v2,0);
-    Node A=new Node(0,"A",numCities-1,'A',initialHeuristic,v1);
+    //Node A=new Node(0,"A",numCities-1,'A',initialHeuristic,v1);
     
     int lengthMst = buildMinimumSpanningTree2(v1,v2);
     cout<<"\n the length of the MST is "<<lengthMst<<"\n";
