9a50fa21ab56077cbac5909f2506ad75e5f646bd
deerishi <deerishi@gmail.com>
2016-01-21 15:42:00 -0500

    adding files

--- CS 686 AI/temp.cpp
+++ CS 686 AI/temp.cpp
@@ -14,12 +14,7 @@ class ai
    
 };
 
-template <typename Container> // we can make this generic for any container [1]
-struct container_hash {
-    std::size_t operator()(Container const& c) const {
-        return boost::hash_range(c.begin(), c.end());
-    }
-};
+
 bool compare( ai v1,  ai v2)
 {
   
