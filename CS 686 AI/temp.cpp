c949419bf4416ae5b51772a66854d6631ec4a7a7
deerishi <deerishi@gmail.com>
2016-01-21 14:23:36 -0500

    adding files

--- CS 686 AI/temp.cpp
+++ CS 686 AI/temp.cpp
@@ -18,30 +18,7 @@ bool compare( ai v1,  ai v2)
     return v1.b<v2.b;
 }
 #include <queue>
-template<
-    class T,
-    class Container = std::vector<T>,
-    class Compare = std::less<typename Container::value_type>
-> class MyQueue : public std::priority_queue<T, Container, Compare>
-{
-public:
-    typedef typename
-        std::priority_queue<
-        T,
-        Container,
-        Compare>::container_type::const_iterator const_iterator;
 
-    const_iterator find(const T&val) const
-    {
-        auto first = this->c.cbegin();
-        auto last = this->c.cend();
-        while (first!=last) {
-            if (*first==val) return first;
-            ++first;
-        }
-        return last;
-    }
-};
 
 int main()
 {
