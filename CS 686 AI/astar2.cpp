25f05e6b595f3a585103f85a5c01c6d59dfd654c
deerishi <deerishi@gmail.com>
2016-01-21 23:26:09 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -358,7 +358,7 @@ int startSearch()
         }
         cout<<"current heuristic is "<<current.hCost<<" last path is "<<current.pathSoFar[current.pathSoFar.length()-1]<<"\n";
         vector<int> vertices=current.citiesLeft;
-        vector<int>::iterator it,it2;
+        vector<int>::iterator it,it3;
         vector<int> nextUnvisitedCities;
         vector<char> namesOfCitiesYettoVisit,namesOfCitiesYettoVisit2;
         vector<char>::iterator it2;
