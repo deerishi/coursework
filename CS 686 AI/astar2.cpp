7aedccc72af8a1909ba86eb0cbd06d21f10ebfac
deerishi <deerishi@gmail.com>
2016-01-21 20:27:50 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -28,7 +28,7 @@ class Node
         }
         
         Node(){};
-        Node(int citynum,string pathSoFar,int citiesNotVisited,char name,int hCost,,int actualCost,int totalCost,vector<int> citiesLeft):citynum(citynum),pathSoFar(pathSoFar),citiesNotVisited(),name(name),cost(cost),citiesLeft(citiesLeft){}
+        Node(int citynum,string pathSoFar,int citiesNotVisited,char name,int hCost,,int actualCost,int totalCost,vector<int> citiesLeft):citynum(citynum),pathSoFar(pathSoFar),citiesNotVisited(),name(name),hCost(hCost),,citiesLeft(citiesLeft){}
         /*{
             this->citynum=citynum;
             this->pathSoFar+=pathSoFar;
