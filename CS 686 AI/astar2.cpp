92032df81f39275fd6d3b3064bf7bf19b061853b
deerishi <deerishi@gmail.com>
2016-01-21 23:25:01 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -226,8 +226,9 @@ int calculateHeuristic(vector<int> vertices,vector<char> nodesLeft,int currentCi
     {
         pcity[i]=*it1; //parent city 
         pdist[i]=INT_MAX; //parent distance;
+                cout<<"pcity["<<i<<"] is "<<pcity[i]<<"\n";
         i++;
-        cout<<"pcity["<<i<<"] is "<<pcity[i]<<"\n";
+
     }
     int mst=buildMinimumSpanningTree2(vertices,nodesLeft);
     int nearestUnvisitedCityDistance=INT_MAX,nearestToSource=INT_MAX,thisDistance1,thisDistance2;
