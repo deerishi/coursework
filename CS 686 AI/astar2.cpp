23a3601018640b1d135b09441dfa00e61a594196
deerishi <deerishi@gmail.com>
2016-01-22 10:43:01 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -75,7 +75,7 @@ class TSP
      
      int CalculateDistance(int x1,int y1,int x2,int y2)
      {
-        return int(sqrt(pow(x1-x2,2)+pow(y1-y2,2)));
+        return int(sqrt(pow(x1-x2,2)+pow(y1-y2,2)) + 0.5 );
      }
      void createOriginalDistanceGraph()
      {
