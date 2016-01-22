31af97edab7fb64a469caccabc2c0da759c8740f
deerishi <deerishi@gmail.com>
2016-01-22 01:11:27 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -210,7 +210,7 @@ int buildMinimumSpanningTree2(vector<int> vertices,vector<char> nodesLeft)
 int calculateHeuristic(vector<int> vertices,vector<char> nodesLeft,int currentCityForExpansion)
 {
     int size=vertices.size();
-    cout<<" the heuristic vector size is "<<siz
+    cout<<" the heuristic vector size is "<<size<<"\n";
     if(size==1)
     {
         vector<int>:: iterator itt=vertices.begin();
