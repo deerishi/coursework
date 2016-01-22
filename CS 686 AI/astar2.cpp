ca17b146eea602621ea6677a13b40f5c8bbbc65b
deerishi <deerishi@gmail.com>
2016-01-22 01:18:17 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -353,7 +353,7 @@ int startSearch()
     {
         current=astar.top();
          astar.pop();
-        cout<<"current is "<<current.citynum<<"with cost "<<current.totalCost<<"\n";
+        cout<<"current is "<<current.citynum<<" with cost "<<current.totalCost<<"\n";
         cout<<"size of priority_queue is "<<astar.size()<<"\n";
         pathSoFar=current.pathSoFar;
         state=current.state;
