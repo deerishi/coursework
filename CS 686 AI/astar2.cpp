8e980997f49b3b0f6e25c79d683458d9e9367545
deerishi <deerishi@gmail.com>
2016-01-22 00:21:27 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -352,7 +352,7 @@ int startSearch()
         {
             //i.e. all cities have been visited, just the tour back to the starting city is left.
             int tcost=current.actualCost + distance(current.citynum,0);
-            cout<<" goal state reached with cost of "<<cost<<"\n";
+            cout<<" goal state reached with cost of "<<tcost<<"\n";
             if(tcost< optimumCost )
             {
                 optimumCost=tcost;
