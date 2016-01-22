ae833ed8d75ff3dabc4f091565b55e942186c718
deerishi <deerishi@gmail.com>
2016-01-22 00:44:53 -0500

    adding files

--- CS 686 AI/astar2.cpp
+++ CS 686 AI/astar2.cpp
@@ -400,7 +400,7 @@ int startSearch()
                 //cout<<"*it3 is "<<x<<" ";
                 nextUnvisitedCities.push_back(x);
             }
-            for(it4=nextUnvisitedCities.begin();it4!=nextUnvisitedCities.end();it4++)
+            f//or(it4=nextUnvisitedCities.begin();it4!=nextUnvisitedCities.end();it4++)
             //cout<<*it4<<" ";
             cout<<"\n";
             for(it4=vertices.begin();it4!=vertices.end();it4++)
